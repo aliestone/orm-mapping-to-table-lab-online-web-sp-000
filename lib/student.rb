dbd9cc0a980dbed2b9107956abc4cb5bd70eff24
Alie Stone <helloaliestone@gmail.com>
2020-10-18 15:15:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,18 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  
+
   def initialize(name, grade)
     @name = name
     @grade = grade
   end
+
+  def self.create(:name, :grade)
+    @name = name
+    @grade = grade
+    save
+  end
+
 
   def self.create_table
     sql =  <<-SQL
